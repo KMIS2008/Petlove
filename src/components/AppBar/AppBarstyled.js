876f250adc45ccb949ed663306c35b00936b2aed
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2024-05-22 19:26:01 +0300

    create Page Home

--- src/components/AppBar/AppBarstyled.js
+++ src/components/AppBar/AppBarstyled.js
@@ -4,8 +4,8 @@ export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 0 auto 30px auto;
+  margin: 0 auto;
   border: 3px solid #f08080;
   background-color: #808080;
-  width: 700px;
+
 `
