6045b974ef7734f9bda4ca80b95033d596ebbce6
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2024-05-31 15:06:32 +0300

    styled  NewList

--- src/components/NewsList/NewsList.styled.js
+++ src/components/NewsList/NewsList.styled.js
@@ -8,6 +8,15 @@ list-style: none;
     display: flex;
     flex-wrap: wrap;
     gap: ${p=>p.theme.spacing(4)};
+    flex: 0 1 calc(50% - 8px); 
+box-sizing: border-box;
+}
+
+
+
+@media (min-width: 1280) {
+
+    flex:0 1 calc(33.33% - 8px)
 }
 
 `
