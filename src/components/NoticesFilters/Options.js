4c3fa9500bc824efe578a8ffdf45dcaf19ae2414
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2024-06-11 16:44:04 +0300

    create Options

---
+++ src/components/NoticesFilters/Options.js
@@ -0,0 +1,33 @@
+export const category=[
+'Show all',
+'Sell',
+'Free',
+'Lost',
+'Found'
+]
+
+export const byGender=[
+'Show all',
+'Unknown',
+'Female',
+'Male',
+'Multiple'
+]
+
+export const byType =[
+'Show all',
+'Dog',
+'Cat',
+'Monkey',
+'Bird',
+'Snake',
+'Turtle',
+'Lizard',
+'Frog',
+'Fish',
+'Ants',
+'Bees',
+'Buterfly',
+'Spider',
+'Scorpion',
+]
