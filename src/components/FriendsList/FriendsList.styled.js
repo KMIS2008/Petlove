3e10eaee6891b60f77bc151903955ddaa47ce029
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2024-06-18 16:54:05 +0300

    styled for FriendPage for desctop and tablet

--- src/components/FriendsList/FriendsList.styled.js
+++ src/components/FriendsList/FriendsList.styled.js
@@ -5,4 +5,13 @@ background: #F9F9F9;
 list-style: none;
 padding: 40px 0;
 
+@media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+}
+
+@media (min-width: 1280px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+}
 `
